c2543b0d8fa0d7684f7015717301eb1e5d2f7083
Cristopher Loyola <loyoladv@gmail.com>
2024-12-27 21:57:19 -0500

    php

--- assets/enviar.php
+++ assets/enviar.php
@@ -1,29 +1,32 @@
 <?php
-if (isset($_POST['submit'])) {
-    // Recoger los datos del formulario
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+
     $monto = $_POST['monto'];
+    $dni = $_POST['dni'];
     $nombres = $_POST['nombres'];
     $distrito = $_POST['distrito'];
     $celular = $_POST['celular'];
     $actividad = $_POST['actividad'];
 
-    // Dirección de correo de destino (tu correo corporativo)// Cambia esto por tu correo real
-    $destinatario = "loyoladv@gmail.com";
+    $destinatario = "info@marakpital.com";
     $asunto = "Prestamo el cliente $nombres";
-
-    $contenido = "Nombre: " . $nombres . "\n";
+    $contenido = "DNI: " . $dni . "\n"; 
+    $contenido .= "Nombre: " . $nombres . "\n";
     $contenido .= "Distrito: " . $distrito . "\n";
     $contenido .= "Celular: " . $celular . "\n";    
     $contenido .= "Actividad: " . $actividad . "\n";    
     $contenido .= "Monto: " . $monto . "\n";
 
-    $header = "From: prestamo@marakpital.com";
+    $headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/plain; charset=UTF-8' . "\r\n";
+    $headers .= 'From: ' . "\r\n";
+    $headers .= 'Reply-To: ' . "\r\n";
 
-    $mail = mail($destinatario, $asunto, $contenido, $header);   
+    $mail = mail($destinatario, $asunto, $contenido, $headers);   
     if ($mail) {
         echo "<script>alert('Gracias por su solicitud, nos pondremos en contacto con usted lo más pronto posible.')</script>";
     } else {
-        echo "<script>alert('No se envio el mensaje.')</script>";
+        echo "<script>alert('No se envió el mensaje.')</script>";
     }
 }
 ?>
